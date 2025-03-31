3aaddc380d51ff65c34fda0d99a54094d771e20b
julianhernao.marin.contractor.gdevtools.com <julianhernao.marin.contractor@gdevtools.com>
2025-03-31 15:54:07 -0500

    Add initial code

---
+++ src/main/java/com/example/spring_boot_hazelcast_cache/SpringBootHazelcastCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_hazelcast_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcastCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHazelcastCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_hazelcast_cache/SpringBootHazelcastCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_hazelcast_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHazelcastCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
